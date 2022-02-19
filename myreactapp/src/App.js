adb10764b27d1fc613cdc4c0dd38de981fa12610
ankitgoyal1357935 <ankitgoyal1357935@gmail.com>
2022-02-19 12:04:13 +0530

    done

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -4,14 +4,14 @@ import Navc from "./component/Navc";
 import Login from "./component/Login";
 import Register from "./component/Register";
 import Slider from "./component/Slider";
+import Carde from "./component/Card";
 function App() {
   return (
   <>
 
    <Navc/>
    <Slider/>
-   <Login/>
-   <Register/>
+    <Carde/>
     </>
   )  
 }
